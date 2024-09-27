9ffca8268e4041dbb898222c861021bdd2d83126
Anish Karki <112316389+anish102@users.noreply.github.com>
2024-09-27 22:26:36 +0545

    Deprecate path operation

--- path_operation_descripion.py
+++ path_operation_descripion.py
@@ -20,7 +20,7 @@ class Item(BaseModel):
 async def create_item(item: Item):
     return item
 
-@app.post("/itemss/", response_model=Item, summary="Create an item")
+@app.post("/itemss/", response_model=Item, summary="Create an item",response_description="The created item",)
 async def create_item(item: Item):
     """
     Create an item with all the information:
@@ -32,3 +32,19 @@ async def create_item(item: Item):
     - **tags**: a set of unique tag strings for this item
     """
     return item
+
+
+@app.get("/items/", tags=["items"])
+async def read_items():
+    return [{"name": "Foo", "price": 42}]
+
+
+@app.get("/users/", tags=["users"])
+async def read_users():
+    return [{"username": "johndoe"}]
+
+
+@app.get("/elements/", tags=["items"], deprecated=True)
+async def read_elements():
+    return [{"item_id": "Foo"}]
+
