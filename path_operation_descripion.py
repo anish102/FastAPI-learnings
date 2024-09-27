78870b79f2fd394536a33c53e728823d2506bd3c
Anish Karki <112316389+anish102@users.noreply.github.com>
2024-09-27 21:05:55 +0545

    Summary and Description

---
+++ path_operation_descripion.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+
+
+class Item(BaseModel):
+    name: str
+    description: str | None = None
+    price: float
+    tax: float | None = None
+    tags: set[str] = set()
+  
