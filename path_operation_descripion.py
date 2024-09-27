85fdf352253c03ca394a6a3459181a7a663da5e6
Anish Karki <112316389+anish102@users.noreply.github.com>
2024-09-27 21:07:52 +0545

    Description from docstring

--- path_operation_descripion.py
+++ path_operation_descripion.py
@@ -10,4 +10,25 @@ class Item(BaseModel):
     price: float
     tax: float | None = None
     tags: set[str] = set()
-  
+
+@app.post(
+    "/items/",
+    response_model=Item,
+    summary="Create an item",
+    description="Create an item with all the information, name, description, price, tax and a set of unique tags",
+)
+async def create_item(item: Item):
+    return item
+
+@app.post("/itemss/", response_model=Item, summary="Create an item")
+async def create_item(item: Item):
+    """
+    Create an item with all the information:
+
+    - **name**: each item must have a name
+    - **description**: a long description
+    - **price**: required
+    - **tax**: if the item doesn't have tax, you can omit this
+    - **tags**: a set of unique tag strings for this item
+    """
+    return item
